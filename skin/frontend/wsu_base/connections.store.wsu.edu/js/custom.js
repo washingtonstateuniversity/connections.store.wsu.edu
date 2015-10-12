cb90be862bf695e9217571d854fb41dcc9d37c80
Jeremy Bass <jeremybass@cableone.net>
2015-10-12 13:59:02 -0700

    sets up to be able to custom define the lightbox

--- skin/frontend/wsu_base/connections.store.wsu.edu/js/custom.js
+++ skin/frontend/wsu_base/connections.store.wsu.edu/js/custom.js
@@ -1,5 +1,36 @@
 (function($){
 	$(document).ready(function(){
+			
+		$('.more-views a').lightbox({
+			gallery: {
+				enabled: true,
+				loop: true,
+			},
+			dialog:{
+				resizeToBestPossibleSize: false,
+				draggable: false,
+				resizable: false,
+				modal: true,
+				onCreate:function(){
+					$('.ui-dialog-titlebar').remove();
+					//$(".ui-dialog-buttonpane").remove();
+					$('body').css({overflow:"hidden"});
+				},
+				onOpen:function(jObj){
+					jObj.prepend('<span class="tabedBox infoClose">X</span>');
+					jObj.find('.infoClose').off().on("click",function(e){
+						//WSU_MAP.util.nullout_event(e);
+						jObj.dialog( "close" );
+					});
+				},
+				onClose: function(){//jObj) {
+					//WSU_MAP.util.close_dialog_modle(obj);
+					//jObj.remove();
+					$('body').css({overflow:"auto"});
+				}
+			},
+		});
 
+			//$('.more-views a').lightbox( "option", "resizeToBestPossibleSize", false );
 	});
 })(jQuery);
